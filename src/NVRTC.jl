a49c60651ec5aac44dad13fbaa87e5f3039245f2
Hiroyuki Shindo <hshindo@outlook.com>
2018-03-25 16:07:36 +0900

    Fix NVRTC

--- src/NVRTC.jl
+++ src/NVRTC.jl
@@ -5,9 +5,9 @@ if is_windows()
 else
     const libnvrtc = Libdl.find_library("libnvrtc")
 end
-const ACTIVE = !isempty(libnvrtc)
+isempty(libnvrtc) && error("NVRTC cannot found.")
 
-if ACTIVE
+function init()
     ref_major = Ref{Cint}()
     ref_minor = Ref{Cint}()
     ccall((:nvrtcVersion,libnvrtc), Cint, (Ptr{Cint},Ptr{Cint}), ref_major, ref_minor)
@@ -15,12 +15,10 @@ if ACTIVE
     minor = Int(ref_minor[])
     const API_VERSION = 1000major + 10minor
     info("NVRTC API $API_VERSION")
-else
-    const API_VERSION = 0
-    warn("NVRTC library cannot be found.")
 end
+init()
 
-macro apicall(f, args...)
+macro nvrtc(f, args...)
     quote
         result = ccall(($f,libnvrtc), Cint, $(map(esc,args)...))
         if result != 0
@@ -35,17 +33,17 @@ function compile(code::String; headers=[], include_names=[], options=[])
     ref = Ref{Ptr{Void}}()
     headers = Ptr{UInt8}[pointer(h) for h in headers]
     include_names = Ptr{UInt8}[pointer(n) for n in include_names]
-    @apicall :nvrtcCreateProgram (Ptr{Ptr{Void}},Cstring,Cstring,Cint,Ptr{Ptr{UInt8}},Ptr{Ptr{UInt8}}) ref code C_NULL length(headers) headers include_names
+    @nvrtc :nvrtcCreateProgram (Ptr{Ptr{Void}},Cstring,Cstring,Cint,Ptr{Ptr{UInt8}},Ptr{Ptr{UInt8}}) ref code C_NULL length(headers) headers include_names
     prog = ref[]
 
     options = Ptr{UInt8}[pointer(o) for o in options]
     try
-        @apicall :nvrtcCompileProgram (Ptr{Void},Cint,Ptr{Ptr{UInt8}}) prog length(options) options
+        @nvrtc :nvrtcCompileProgram (Ptr{Void},Cint,Ptr{Ptr{UInt8}}) prog length(options) options
     catch
         ref = Ref{Csize_t}()
-        @apicall :nvrtcGetProgramLogSize (Ptr{Void},Ptr{Csize_t}) prog ref
+        @nvrtc :nvrtcGetProgramLogSize (Ptr{Void},Ptr{Csize_t}) prog ref
         log = Array{UInt8}(Int(ref[]))
-        @apicall :nvrtcGetProgramLog (Ptr{Void},Ptr{UInt8}) prog log
+        @nvrtc :nvrtcGetProgramLog (Ptr{Void},Ptr{UInt8}) prog log
         println()
         println("Error log:")
         println(String(log))
@@ -53,12 +51,12 @@ function compile(code::String; headers=[], include_names=[], options=[])
     end
 
     ref = Ref{Csize_t}()
-    @apicall :nvrtcGetPTXSize (Ptr{Void},Ptr{Csize_t}) prog ref
+    @nvrtc :nvrtcGetPTXSize (Ptr{Void},Ptr{Csize_t}) prog ref
     ptxsize = ref[]
 
     ptx = Array{UInt8}(ptxsize)
-    @apicall :nvrtcGetPTX (Ptr{Void},Ptr{UInt8}) prog ptx
-    @apicall :nvrtcDestroyProgram (Ptr{Ptr{Void}},) Ref{Ptr{Void}}(prog)
+    @nvrtc :nvrtcGetPTX (Ptr{Void},Ptr{UInt8}) prog ptx
+    @nvrtc :nvrtcDestroyProgram (Ptr{Ptr{Void}},) Ref{Ptr{Void}}(prog)
 
     String(ptx)
 end
