a4a0b9655c1fce5d81e7cda32aa2b86657c4d23f
Hiroyuki Shindo <hshindo@outlook.com>
2018-03-25 18:38:12 +0900

    Revert Array{Int} and CuArray{Cint} mapping

--- src/nvml/NVML.jl
+++ src/nvml/NVML.jl
@@ -7,7 +7,7 @@ if is_windows()
 else
     const libnvml = Libdl.find_library("libnvml")
 end
-const ACTIVE = !isempty(libnvml)
+isempty(libnvml) && warn("NVML cannot be found.")
 
 function checkresult(result::Cint)
     if result != 0
@@ -16,38 +16,32 @@ function checkresult(result::Cint)
     end
 end
 
-if ACTIVE
+function init()
     result = ccall((:nvmlInit_v2,libnvml), Cint, ())
     checkresult(result)
 
     ref = Array{Cchar}(80)
     result = ccall((:nvmlSystemGetNVMLVersion,libnvml), Cint, (Ptr{Cchar},Cuint), ref, 80)
     checkresult(result)
-    
+
     const API_VERSION = unsafe_string(pointer(ref))
     info("NVML $API_VERSION")
-else
-    warn("NVML API cannot be found.")
 end
 
 include("define.jl")
 
-macro apicall(f, args...)
+macro nvml(f, args...)
     f = get(DEFINE, f.args[1], f.args[1])
-    if ACTIVE
-        quote
-            result = ccall(($(QuoteNode(f)),libnvml), Cint, $(map(esc,args)...))
-            checkresult(result)
-        end
+    quote
+        result = ccall(($(QuoteNode(f)),libnvml), Cint, $(map(esc,args)...))
+        checkresult(result)
     end
 end
 
-macro apicall_nocheck(f, args...)
+macro nvml_nocheck(f, args...)
     f = get(DEFINE, f.args[1], f.args[1])
-    if ACTIVE
-        quote
-            ccall(($(QuoteNode(f)),libnvml), Cint, $(map(esc,args)...))
-        end
+    quote
+        ccall(($(QuoteNode(f)),libnvml), Cint, $(map(esc,args)...))
     end
 end
 
--- src/nvml/device.jl
+++ src/nvml/device.jl
@@ -2,7 +2,7 @@ export free_devices
 
 function gethandle(dev::Int)
     ref = Ref{Ptr{Void}}()
-    @apicall :nvmlDeviceGetHandleByIndex (Cuint,Ptr{Void}) dev ref
+    @nvml :nvmlDeviceGetHandleByIndex (Cuint,Ptr{Void}) dev ref
     ref[]
 end
 
@@ -13,7 +13,7 @@ function running_processes(dev::Int)
     h = gethandle(dev)
     ref_count = Ref{Ptr{Cuint}}()
     ref_infos = Ref{Ptr{Void}}()
-    @apicall :nvmlDeviceGetComputeRunningProcesses (Ptr{Void},Ptr{Cuint},Ptr{Void}) h ref_count ref_infos
+    @nvml :nvmlDeviceGetComputeRunningProcesses (Ptr{Void},Ptr{Cuint},Ptr{Void}) h ref_count ref_infos
 end
 
 function free_devices(maxcount::Int)
@@ -22,7 +22,7 @@ function free_devices(maxcount::Int)
         length(devs) >= maxcount && break
         # setdevice(i) do
         h = gethandle(i)
-        res = @apicall_nocheck :nvmlDeviceGetComputeRunningProcesses (Ptr{Void},Ptr{Cuint},Ptr{Void}) h Ref{Cuint}(0) C_NULL
+        res = @nvml_nocheck :nvmlDeviceGetComputeRunningProcesses (Ptr{Void},Ptr{Cuint},Ptr{Void}) h Ref{Cuint}(0) C_NULL
         (res == 0 || res == 7) && push!(devs,i)
     end
     devs
